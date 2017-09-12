1691b0e7b002452a171d24f8180c9303cb08975c
Brett Starks <brettz.starks@gmail.com>
2017-09-11 19:20:26 -0600

    Adding initial book example .java and .class files

---
+++ Book Examples/Example2.java
@@ -0,0 +1,13 @@
+class Example2 {
+	public static void main(String args[]){
+		int var1;
+		int var2;
+
+		var1 = 1024;
+		System.out.println("var1 contains " + var1);
+
+		var2 = var1 / 2;
+		System.out.print("var2 is half of var1, which is: ");
+		System.out.println(var2);
+	}
+}
---
+++ Book Examples/IfDemo.java
@@ -0,0 +1,27 @@
+class IfDemo {
+	public static void main(String args[]) {
+		int a, b, c;
+
+		a = 2;
+		b = 3;
+
+		if(a < b) System.out.println("a is less than b.");
+		if(a == b) System.out.println("You're not gonna see this one, Davie!");
+
+		System.out.println();
+
+		c = a - b;
+
+		System.out.println("c contains " + c);
+		if(c >= 0) System.out.println("c is non-negative.");
+		if(c < 0) System.out.println("c is non-non-negative.");
+		
+		System.out.println();
+
+		c = b-a;
+
+		System.out.println("c contains " + c);
+		if(c >= 0) System.out.println("c is non-negative.");
+		if(c < 0) System.out.println("c is non-non-negative.");
+	}
+}
