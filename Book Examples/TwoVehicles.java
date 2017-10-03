f09e8ac5784685aa50005ed9e2e0af1c63c46b67
Brett Starks <brettz.starks@gmail.com>
2017-10-03 09:42:58 -0600

    Added method to TwoVehicles

--- Book Examples/TwoVehicles.java
+++ Book Examples/TwoVehicles.java
@@ -3,15 +3,15 @@ class Vehicle {
     int fuelcap;
     int mpg;
     
-    
+    void range(){
+        System.out.println("Range is " + fuelcap * mpg);
+    }
 }
 
 class TwoVehicles {
     public static void main(String args[]) {
        Vehicle minivan = new Vehicle();
        Vehicle sportscar = new Vehicle();
-    
-        int range1, range2;
 
         minivan.passengers = 7;
         minivan.fuelcap = 16;
@@ -21,11 +21,10 @@ class TwoVehicles {
         sportscar.fuelcap = 14;
         sportscar.mpg = 12;
 
-        range1 = minivan.fuelcap * minivan.mpg;
-        range2 = sportscar.fuelcap * sportscar.mpg;
-
-        System.out.println("Minivan can carry " + minivan.passengers + " with a range of " + range1);
-        System.out.println("Sportscar can carry " + sportscar.passengers + " with a range of " + range2);
+        System.out.print("Minivan can carry " + minivan.passengers + ". ");
+        minivan.range();
+        System.out.print("Sportscar can carry " + sportscar.passengers + ". ");
+        sportscar.range();
     }
     
     
